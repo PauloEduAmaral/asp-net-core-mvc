ba1acb1cd0d1821e40ed78b98f5a2f267e76f012
PauloEduAmaral <paulo.e.amaral@gmail.com>
2020-07-28 15:23:09 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -33,6 +33,22 @@ namespace SalesWebMvc.Controllers
             return View(viewModel);
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) return NotFound();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
